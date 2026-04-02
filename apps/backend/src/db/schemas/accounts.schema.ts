e992016f3f321d0306ef43a16028f70c05a9fd0b
Eduardo Ferras <edferras@gmail.com>
2026-04-02 14:06:48 -0300

    refactor(backend): implement automatic snake_case naming convention accounts schema

--- apps/backend/src/db/schemas/accounts.schema.ts
+++ apps/backend/src/db/schemas/accounts.schema.ts
@@ -6,26 +6,26 @@ import { index, pgTable, text, timestamp } from "drizzle-orm/pg-core";
 export const accounts = pgTable(
 	"accounts",
 	{
-		id: text("id").primaryKey(),
-		accountId: text("account_id").notNull(),
-		providerId: text("provider_id").notNull(),
-		userId: text("user_id")
+		id: text().primaryKey(),
+		accountId: text().notNull(),
+		providerId: text().notNull(),
+		userId: text()
 			.notNull()
 			.references(() => users.id, { onDelete: "cascade" }),
-		accessToken: text("access_token"),
-		refreshToken: text("refresh_token"),
-		idToken: text("id_token"),
-		accessTokenExpiresAt: timestamp("access_token_expires_at", {
+		accessToken: text(),
+		refreshToken: text(),
+		idToken: text(),
+		accessTokenExpiresAt: timestamp({
 			withTimezone: true,
 		}),
-		refreshTokenExpiresAt: timestamp("refresh_token_expires_at", {
+		refreshTokenExpiresAt: timestamp({
 			withTimezone: true,
 		}),
-		scope: text("scope"),
-		password: text("password"),
+		scope: text(),
+		password: text(),
 		...timestamps,
 	},
-	(table) => [index("accounts_userId_idx").on(table.userId)],
+	(table) => [index("accounts_user_id_idx").on(table.userId)],
 );
 
 export const accountsRelations = relations(accounts, ({ one }) => ({
