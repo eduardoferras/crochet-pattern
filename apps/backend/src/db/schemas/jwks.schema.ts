9a3687563be76be089141201b37345839565b59d
Eduardo Ferras <edferras@gmail.com>
2026-04-04 22:27:44 -0300

    refactor id replace with idColumn helper in jwks schema

--- apps/backend/src/db/schemas/jwks.schema.ts
+++ apps/backend/src/db/schemas/jwks.schema.ts
@@ -1,7 +1,8 @@
+import { idColumn } from "@db/helpers/columns.helpers.ts";
 import { pgTable, text, timestamp } from "drizzle-orm/pg-core";
 
 export const jwks = pgTable("jwks", {
-	id: text().primaryKey(),
+	id: idColumn().primaryKey(),
 	publicKey: text().notNull(),
 	privateKey: text().notNull(),
 	createdAt: timestamp({ withTimezone: true }).notNull(),
