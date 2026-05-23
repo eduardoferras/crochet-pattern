bcc56384fc046fab71b659b8e77394b843ae0360
Eduardo Ferras <edferras@gmail.com>
2026-05-22 22:30:28 -0300

    refactor: converter feedback controller to class and update feedback routes

--- apps/backend/src/controllers/feedback.controller.ts
+++ apps/backend/src/controllers/feedback.controller.ts
@@ -2,20 +2,24 @@ import { addFeedbackJob } from "@jobs/feedback.job.ts";
 import type { FeedbackSchema } from "@validations/feedback.validation.ts";
 import type { Request, Response } from "express";
 
-export const sendFeedback = async (req: Request, res: Response) => {
-	try {
-		const data: FeedbackSchema = req.body;
+export class FeedbackController {
+	sendFeedback = async (req: Request, res: Response) => {
+		try {
+			const data: FeedbackSchema = req.body;
 
-		await addFeedbackJob(data);
+			await addFeedbackJob(data);
 
-		return res
-			.status(200)
-			.json({ message: "Feedback received", feedback: data.feedback });
-	} catch (error) {
-		console.error("Error sending feedback:", error);
+			return res
+				.status(200)
+				.json({ message: "Feedback received", feedback: data.feedback });
+		} catch (error) {
+			console.error("Error sending feedback:", error);
 
-		return res
-			.status(500)
-			.json({ message: "Internal server error sending feedback" });
-	}
-};
+			return res
+				.status(500)
+				.json({ message: "Internal server error sending feedback" });
+		}
+	};
+}
+
+export const feedbackController = new FeedbackController();
--- apps/backend/src/routes/feedback.routes.ts
+++ apps/backend/src/routes/feedback.routes.ts
@@ -1,10 +1,14 @@
-import { sendFeedback } from "@controllers/feedback.controller.ts";
+import { feedbackController } from "@controllers/feedback.controller.ts";
 import validateBody from "@middlewares/validateBody.middleware.ts";
 import { feedbackZodSchema } from "@validations/feedback.validation.ts";
 import { Router } from "express";
 
 const feedbackRouter = Router();
 
-feedbackRouter.post("/", validateBody(feedbackZodSchema), sendFeedback);
+feedbackRouter.post(
+	"/",
+	validateBody(feedbackZodSchema),
+	feedbackController.sendFeedback,
+);
 
 export default feedbackRouter;
