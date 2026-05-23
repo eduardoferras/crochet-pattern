df4e24140d6c1b474f071358964483fcd6bd14e8
Eduardo Ferras <edferras@gmail.com>
2026-05-22 22:30:28 -0300

    refactor: converter routes to class and update App

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -1,41 +1,43 @@
 import path from "node:path";
 import { fileURLToPath } from "node:url";
-import routes from "@routes/index.ts";
+import { AppRoutes } from "@routes/index.ts";
 import cors from "cors";
 import express, { type Application } from "express";
 import "@/workers/index.ts";
 import { corsOptions } from "@config/cors.config.ts";
 import { initCrons } from "@crons/index.ts";
-import authRouter from "@routes/auth.route.ts";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default class App {
 	public readonly express: Application;
+	private readonly routes: AppRoutes;
 
 	constructor() {
 		this.express = express();
 		this.express.use(cors(corsOptions));
 
-		this.initializeAuth();
+		this.routes = new AppRoutes(this.express);
+
+		this.initializeAuthRoutes();
 		this.initializeMiddlewares();
 		this.initializeRoutes();
 		this.initializeStaticFiles();
 		initCrons();
 	}
 
-	private initializeAuth(): void {
-		this.express.use("/auth", authRouter);
+	private initializeAuthRoutes(): void {
+		this.routes.initializeAuthRoutes();
+	}
+
+	private initializeRoutes(): void {
+		this.routes.initializeRoutes();
 	}
 
 	private initializeMiddlewares(): void {
 		this.express.use(express.json({ limit: "5mb" }));
 		this.express.use(express.urlencoded({ limit: "5mb", extended: true }));
-	}
-
-	private initializeRoutes(): void {
-		this.express.use(routes);
 	}
 
 	private initializeStaticFiles(): void {
--- apps/backend/src/routes/index.ts
+++ apps/backend/src/routes/index.ts
@@ -1,12 +1,19 @@
+import authRouter from "@routes/auth.route.ts";
 import feedbackRouter from "@routes/feedback.routes.ts";
 import healthRouter from "@routes/health.routes.ts";
 import productRouter from "@routes/product.routes.ts";
-import { Router } from "express";
+import type { Application } from "express";
 
-const router = Router();
+export class AppRoutes {
+	constructor(private readonly app: Application) {}
 
-router.use("/health", healthRouter);
-router.use("/products", productRouter);
-router.use("/feedback", feedbackRouter);
+	initializeAuthRoutes() {
+		this.app.use("/auth", authRouter);
+	}
 
-export default router;
+	initializeRoutes() {
+		this.app.use("/health", healthRouter);
+		this.app.use("/products", productRouter);
+		this.app.use("/feedback", feedbackRouter);
+	}
+}
